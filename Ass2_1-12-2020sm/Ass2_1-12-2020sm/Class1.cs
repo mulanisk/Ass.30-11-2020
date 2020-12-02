a1ac7f751509c0a3aaa85e41ec793303f86f6749
mulanisk <55908860+mulanisk@users.noreply.github.com>
2020-12-02 15:50:30 +0530

    Add files via upload

---
+++ Ass2_1-12-2020sm/Ass2_1-12-2020sm/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass2_1_12_2020sm
+{
+    class BankOps
+    {
+       
+            public int NetBalance { get; set; }
+        
+            public void Overbalance(int p)
+            {
+            
+                double tax = p * 0.18;
+                System.Windows.Forms.MessageBox.Show("The Total amount to be paid is " + tax);
+
+            }
+            public void Underblance()
+            {
+                System.Windows.Forms.MessageBox.Show("Please maintain the minimum balance amount of Rs.5000");
+            }
+        }
+}
---
+++ Ass2_1-12-2020sm/Ass2_1-12-2020sm/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ass2_1_12_2020sm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BankOps b = new BankOps();
+            b.NetBalance = Convert.ToInt32(textBox1.Text);
+
+
+
+            if (b.NetBalance >= 100000)
+            {
+                b.Overbalance(b.NetBalance);
+            }
+            else if (b.NetBalance <= 5000)
+            {
+                b.Underblance();
+            }
+            else
+            {
+                MessageBox.Show("The amount is exempted from tax");
+            }
+        }
+    }
+}
